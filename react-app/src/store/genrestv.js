3072011c3c65573a5af3626a33742245f815e6e8
Alexis West <alexishwest98@gmail.com>
2023-04-03 21:53:24 -0700

    ready for react components tmo tv shows

---
+++ react-app/src/store/genrestv.js
@@ -0,0 +1,31 @@
+// const GETTVGENRES = 'movies/getGenreMovies'
+
+// /* ___________ Action Creators   ___________ */
+// export const getMoviesGenresAction = (genres) => {
+//     return {
+//         type: GETMOVIESGENRES,
+//         genres
+//     }
+// }
+
+// /* ___________ T H U N K S   ___________ */
+// export const getMoviesGenresThunk = (movie_id) => async (dispatch) => {
+//     const response = await fetch(`/api/genre/movie/${movie_id}`)
+//     if (response.ok) {
+//         const genres = await response.json()
+//         dispatch(getMoviesGenresAction(genres))
+//         return genres;
+//     }
+// }
+
+// /* ___________ Tasks Reducer   ___________ */
+// export default function moviesGenresReducer(state={}, action) {
+//     let newState = {};
+//     switch(action.type){
+//         case GETMOVIESGENRES:
+//             action.genres.forEach(genre => newState[genre.genre_id] = genre)
+//             return newState;
+//         default:
+//              return state
+//     }
+// }
